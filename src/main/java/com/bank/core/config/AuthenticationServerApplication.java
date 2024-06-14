69157b633d623871e6b1bd586bacc19b7539cdd2
Marsh <dsilvamarsh@gmail.com>
2024-06-14 20:23:12 +0530

    Initial commit

---
+++ src/main/java/com/bank/core/config/AuthenticationServerApplication.java
@@ -0,0 +1,13 @@
+package com.bank.core.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServerApplication.class, args);
+	}
+
+}
